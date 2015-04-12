014e0a3280f7c3f65895c2f4142f29f0ca90d4b6
jeroenstevens <jeroenstevens@live.nl>
2015-04-12 21:12:34 +0200

    Reorder nodes.

--- app/src/main/java/com/shinav/mathapp/firebase/FirebaseNodes.java
+++ app/src/main/java/com/shinav/mathapp/firebase/FirebaseNodes.java
@@ -3,15 +3,18 @@ package com.shinav.mathapp.firebase;
 public abstract class FirebaseNodes {
 
     public interface Nodes {
+        String TUTORIALS = "tutorials";
+        String TUTORIAL_FRAMES = "tutorial_frames";
+
         String STORYBOARD = "storyboards";
         String STORYBOARD_FRAMES = "storyboard_frames";
+
         String CONVERSATIONS = "conversations";
         String CONVERSATION_LINES = "conversation_lines";
+
         String QUESTIONS = "questions";
         String APPROACHES = "approaches";
         String APPROACH_PARTS = "approach_parts";
-        String TUTORIALS = "tutorials";
-        String TUTORIAL_FRAMES = "tutorial_frames";
     }
 
 }
