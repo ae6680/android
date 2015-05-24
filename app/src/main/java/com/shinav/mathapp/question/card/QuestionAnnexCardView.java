af69244508cd2dbc85fff105b055a24a77aeb8c6
jeroenstevens <jeroenstevens@live.nl>
2015-05-24 21:00:16 +0200

    [#92392880] Handle no internet connection for question annexes.

--- app/src/main/java/com/shinav/mathapp/question/card/QuestionAnnexCardView.java
+++ app/src/main/java/com/shinav/mathapp/question/card/QuestionAnnexCardView.java
@@ -1,17 +1,23 @@
 package com.shinav.mathapp.question.card;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.shinav.mathapp.R;
 import com.shinav.mathapp.card.Card;
 import com.squareup.picasso.Picasso;
 
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class QuestionAnnexCardView extends Card {
 
     @InjectView(R.id.annex_image_view) ImageView imageView;
+    @InjectView(R.id.internet_not_found) LinearLayout internetNotFound;
+
+    private String annexImageUrl;
 
     public QuestionAnnexCardView(Context context) {
         super(context);
@@ -23,9 +29,28 @@ public class QuestionAnnexCardView extends Card {
     }
 
     public void setAnnexImageUrl(String imageUrl) {
-        Picasso.with(this.getContext())
-                .load(imageUrl)
-                .into(imageView);
+        if (isOnline()) {
+            Picasso.with(this.getContext())
+                    .load(imageUrl)
+                    .into(imageView);
+        } else {
+            internetNotFound.setVisibility(VISIBLE);
+            annexImageUrl = imageUrl;
+        }
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager)
+                this.getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null &&
+                cm.getActiveNetworkInfo().isConnectedOrConnecting();
+    }
+
+    @OnClick(R.id.retry_internet_connection)
+    public void onRetryInternetConnectionClicked() {
+        internetNotFound.setVisibility(GONE);
+        setAnnexImageUrl(annexImageUrl);
     }
 
 }
